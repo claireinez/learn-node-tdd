795e72052fabbab291c4cb40271852f7f9736693
sohilpandya <sohilpandya@me.com>
2015-09-29 11:38:08 +0100

    added few files for testing with tests and travis

--- server.js
+++ server.js
@@ -5,13 +5,18 @@ var port = process.env.PORT || 8000;
 
 function handler(request, response) {
 	var url = request.url;
-	if (url.length === 1) {
+	//if (url.length === 1) {
 		response.writeHead(200, {"Content-Type": "text/html"});
 		response.end("HELLO WORLD!");
-	}
+	// }
+	// else if (url.length === name) {
+	//
+	//
+	//
+	// }
 }
 
-http.createServer(handler);//.listen(port);
+//http.createServer(handler);//.listen(port); do not need it for shot.
 
 console.log('node http server listening on http://localhost:' + port);
 
--- test/tests.js
+++ test/tests.js
@@ -4,11 +4,34 @@ var tape = require('tape');
 var shot = require('shot');
 var handler = require('../server.js');
 
+
+
 tape("check to see if the server is running ok", function (t) {
 
   shot.inject(handler, {method: 'get', url: '/'},function(res){
     var result = res.statusCode;
     t.equal(result, 200, "success!");
+    t.end();
+  });
+});
+
+// tape("Post request takes user to postatmebro", function (t) {
+//
+//   shot.inject(handler, {method: 'post', url: '/post-at-me-bro'},function(res){
+//     var result = res.statusCode;
+//     t.equal(result, 200, "success!");
+//     t.end();
+//   });
+// });
+
+tape("Is the name of the person in H1 tag being returned in the URL after /name/(NAME OF PERSON)", function(t){
+
+  // var url = request.url;
+  // var name = url.split('/')[2].toString();
+  shot.inject(handler, {method: 'get', url: '/name/sohil/'},function(res){
+    var result = (res.payload.indexOf('<h1>sohil</h1>') !== -1);
+    // var documentName = document.getElementsByTagName('h1').innerText;
+    t.equal(result,true, "success!");
     t.end();
   });
 });
